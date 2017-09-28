de83570bf0eb6c5671446ec093607229ca07ad7c
JonH <jheal006@gmail.com>
2017-09-27 21:54:45 -0700

    mac update

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3307,
+  port: 3306,
   host: "localhost",
   user: "root",
-  password: "Zaq!2wsxcv",
+  password: "hkg36c",
   database: "burgers_db"
 });
 
--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
 
 router.post("/", function(req, res) {
   burger.create([
-    "name", "devour"
+    "name", "devoured"
   ], [
-    req.body.name, req.body.devour
+    req.body.burger_name, req.body.devoured
   ], function() {
     res.redirect("/");
   });
@@ -32,19 +32,13 @@ router.put("/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    devour: req.body.devour
+    devoured: req.body.devoured
   }, condition, function() {
     res.redirect("/");
   });
 });
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function() {
-    res.redirect("/");
-  });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
